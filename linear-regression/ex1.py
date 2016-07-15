53ebebdaa34216f66b3e7c40aa928c4f21932827
augustles <1927064778@qq.com>
2016-07-15 18:02:28 +0800

    note

--- linear-regression/ex1.py
+++ linear-regression/ex1.py
@@ -6,11 +6,16 @@
 @Date: 2015/12/20
 """
 '''
-使用线性回归模型预测房价
+1. 使用线性回归模型预测房价
+2. 预测下周有多少观众
+3. 替换数据中缺失值
 '''
+
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import linear_model
+from sklearn import linear_model  # 导入线性回归
+
+# pd读取csv, return x,y列表
 
 
 def get_data(file_name):
@@ -27,13 +32,13 @@ def get_data(file_name):
 # 返回θ0、θ1和预测出的价格值。
 def linear_model_main(X_param, Y_param, predict_value):
     # 构建线性回归模型
-    regr = linear_model.LinearRegression()
-    regr.fit(X_param, Y_param)
+    regr = linear_model.LinearRegression()  # 使用线性回归
+    regr.fit(X_param, Y_param)  # 对输入和输出进行一次fit，训练出一个模型
     # 预测
     predict_outcome = regr.predict(predict_value)
     predictions = {}
     predictions['intercept'] = regr.intercept_
-    predictions['coefficient'] = regr.coef_
+    predictions['coefficient'] = regr.coef_  # 系数矩阵
     predictions['predicted_value'] = predict_outcome
     return predictions
 
@@ -51,29 +56,43 @@ def show_linear_line(X_param, Y_param):
     regr = linear_model.LinearRegression()
     regr.fit(X_param, Y_param)
 
-    plt.scatter(X_param, Y_param, color ='blue')
-    plt.plot(X_param, regr.predict(X_param),color='red', linewidth=2)
+    plt.scatter(X_param, Y_param, color='blue')
+    plt.plot(X_param, regr.predict(X_param), color='red', linewidth=2)
     plt.xticks(())
     plt.yticks(())
     plt.show()
 
-show_linear_line(X_param, Y_param)
-predict_value = 700
-result = linear_model_main(X_param, Y_param, predict_value)
-print("Intercept value: ", result['intercept'])
-print("coefficient: ", result['coefficient'])
-print("Predicted value: ", result['predicted_value'])
+# show_linear_line(X_param, Y_param)
+
+data = pd.read_csv('http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv', index_col=0)
+feature_cols = ['TV', 'Radio', 'Newspaper']
+X = data[feature_cols]
+print X.head()
+y = data['Sales']
+# print y
+lr = linear_model.LinearRegression()
+lr.fit(X, y)
+print lr.intercept_
+print lr.coef_
+print zip(feature_cols, lr.coef_)
+# lr.predict([])
+
+# predict_value = 700
+# result = linear_model_main(X_param, Y_param, predict_value)
+# print("Intercept value: ", result['intercept'])
+# print("coefficient: ", result['coefficient'])
+# print("Predicted value: ", result['predicted_value'][0])
 
 
-# 显示拟合直线
-def show_linear_line(X_param, Y_param):
-    regr = linear_model.LinearRegression()
-    regr.fit(X_param, Y_param)
+# # 显示拟合直线
+# def show_linear_line(X_param, Y_param):
+#     regr = linear_model.LinearRegression()
+#     regr.fit(X_param, Y_param)
 
-    plt.scatter(X_param, Y_param, color ='blue')
-    plt.plot(X_param, regr.predict(X_param),color='red', linewidth=2)
-    plt.xticks(())
-    plt.yticks(())
-    plt.show()
+#     plt.scatter(X_param, Y_param, color='blue')
+#     plt.plot(X_param, regr.predict(X_param), color='red', linewidth=2)
+#     plt.xticks(())
+#     plt.yticks(())
+#     plt.show()
 
-show_linear_line(X_param, Y_param)
+# show_linear_line(X_param, Y_param)
